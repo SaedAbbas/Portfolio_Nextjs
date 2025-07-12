7748c6b816c4781aea50cc0877ad9cce0ac8f7d2
saed abbas <saedabbas0@gmail.com>
2025-07-12 17:03:24 +0300

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <main className="container mx-auto p-4 bg-amber-200">
-      <h1>Welcome to Saed Abbas Portfolio</h1>
-      <p>This is a modern and minimalist portfolio website.</p>
+    <main className="mx-auto p-4 bg-black-100 text-white justify-center items-center flex flex-col overflow-hidden sm:px-10 px-5">
+      <div className="">
+        <h1>Welcome to Saed Abbas Portfolio</h1>
+      </div>
     </main>
   );
 }
