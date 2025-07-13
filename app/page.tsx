5d90148794240d86277a204015ae5e9336e7f0e6
saed abbas <saedabbas0@gmail.com>
2025-07-13 15:24:01 +0300

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "@/compenents/Hero";
+
 export default function Home() {
   return (
     <main className="mx-auto p-4 bg-black-100 text-white justify-center items-center flex flex-col overflow-hidden sm:px-10 px-5">
       <div className="">
-        <h1>Welcome to Saed Abbas Portfolio</h1>
+        <Hero />
       </div>
     </main>
   );
