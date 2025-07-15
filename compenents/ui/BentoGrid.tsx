871660b1aeda3c4adaeee7771729532e86649013
saed abbas <saedabbas0@gmail.com>
2025-07-15 15:17:22 +0300

    new commit

--- compenents/ui/BentoGrid.tsx
+++ compenents/ui/BentoGrid.tsx
@@ -83,6 +83,16 @@ export const BentoGridItem = ({
                     </BackgroundGradientAnimation>
                 )
             }
+            <div
+              className={cn(
+                titleClassName,
+                "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+              )}>
+                <div className="">
+                  {description}
+                </div>
+
+            </div>
         </div>
       <div className="transition duration-200 group-hover/bento:translate-x-2">
         <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
