498adc2c02d9dddc370a8e68ee85ac233e0b66b2
saed abbas <saedabbas0@gmail.com>
2025-07-15 15:23:46 +0300

    new commit

--- compenents/ui/BentoGrid.tsx
+++ compenents/ui/BentoGrid.tsx
@@ -88,20 +88,15 @@ export const BentoGridItem = ({
                 titleClassName,
                 "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
               )}>
-                <div className="">
+                <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
                   {description}
                 </div>
+        <div className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}>
+          {title}
+        </div>
 
             </div>
         </div>
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-          {title}
-        </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
-        </div>
-      </div>
     </div>
   );
 };
