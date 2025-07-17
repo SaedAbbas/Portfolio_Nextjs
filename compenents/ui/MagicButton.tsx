3503159fa8e555de51327b8b539c8947bf819feb
saed abbas <saedabbas0@gmail.com>
2025-07-17 15:56:31 +0300

    new commit

--- compenents/ui/MagicButton.tsx
+++ compenents/ui/MagicButton.tsx
@@ -4,14 +4,15 @@ type MagicButtonProps = {
   title:string,
   icon:React.ReactNode,
   position?:string,
+  handleClick:() => void
   otherClasses?:string,
 }
 
-const MagicButton = ({title,icon,position,otherClasses}:MagicButtonProps) => {
+const MagicButton = ({title,icon,position,handleClick,otherClasses}:MagicButtonProps) => {
 
   return (
     <div>
-      <button className="relative w-full inline-flex h-12 overflow-hidden rounded-2xl p-[1px] md:w-60 md:top-10">
+      <button onClick={handleClick} className="relative w-full inline-flex h-12 overflow-hidden rounded-2xl p-[1px] md:w-60 md:top-10">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
         <span className={`inline-flex h-full w-full gap-3 cursor-pointer items-center justify-center rounded-2xl bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl ${otherClasses}`}>
           {position === 'left' && icon}
