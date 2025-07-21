1b7d0e6ad7d9f4a55ff2682869b02d06bcd10c03
saed abbas <saedabbas0@gmail.com>
2025-07-21 11:06:50 +0300

    test

--- compenents/ui/RecentProject.tsx
+++ compenents/ui/RecentProject.tsx
@@ -11,53 +11,7 @@ const RecentProject = () => {
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center  p-4 gap-x-24 gap-y-8 mt-10">
-        {projects.map((project) => (
-          <div
-            key={project.id}
-            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] sm:w-[570px] flex items-center justify-center w-[80vw]"
-          >
-            <PinContainer title={project.link} href={project.link}>
-              <div className="flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[30vh] h-[20vh] mb-10">
-                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
-                  <Image src="/bg.png" alt="bg-img" fill />
-                </div>
-                <Image
-                  className="z-10 absolute bottom-0"
-                  src={project.img}
-                  alt={project.title}
-                  fill
-                />
-              </div>
-              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
-                {project.title}
-              </h1>
-              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
-                {project.des}
-              </p>
-              <div className="flex items-center justify-between mt-7 mb-3 ">
-                <div className="fle items-center">
-                  {project.iconLists.map((icon, index) => (
-                    <div
-                      key={icon}
-                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                      style={{ transform: `translateX(-${5 * index * 2}px)` }}
-                    >
-                      <img src={icon} alt={icon} className="py-2" />
-                    </div>
-                  ))}
-                </div>
-                <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
-                  </p>
-                  <FaLocationArrow className="ms-3" color="#CBACF9" />
-                </div>
-              </div>
-            </PinContainer>
-          </div>
-        ))}
-      </div>
+
     </div>
   );
 };
