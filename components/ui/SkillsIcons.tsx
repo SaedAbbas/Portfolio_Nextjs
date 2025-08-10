1cb2e8dbf7dbb401c61e352827ce641d627ad15f
saed abbas <saedabbas0@gmail.com>
2025-08-10 10:45:53 +0300

    new commit

--- components/ui/SkillsIcons.tsx
+++ components/ui/SkillsIcons.tsx
@@ -34,7 +34,7 @@ const SkillsIcons = () => {
         className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-6 max-w-5xl mx-auto"
         variants={containerVariants}
         initial="hidden"
-        animate="visible"
+        whileInView='visible'
       >
         {skills.map((skill, index) => (
           <motion.div
